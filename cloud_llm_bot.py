ce5afa9c22e1140bc47d369942d20e8236fd5891
Woobin Choi <khuam1216@gmail.com>
2024-01-08 23:12:47 +0900

    Update cloud_llm_bot.py

--- cloud_llm_bot.py
+++ cloud_llm_bot.py
@@ -15,8 +15,13 @@ from fastapi import FastAPI
 from langserve import add_routes
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.llms.base import LLM
-
+import streamlit as st
+from streamlit_chat import message
 from langchain.callbacks.manager import CallbackManagerForLLMRun
+import pandas as pd
+
+# HCX 토큰 계산기 API 호출
+from hcx_token_cal import token_completion_executor
 
 
 ##################################################################################
@@ -119,15 +124,22 @@ new_docsearch = Chroma(persist_directory=os.path.join(db_save_path, "cloud_bot_2
                         embedding_function=embeddings)
 retriever = new_docsearch.as_retriever(
                                         search_type="mmr",
-                                        search_kwargs={'k': 3, 'fetch_k': 30}
+                                        search_kwargs={'k': 2, 'fetch_k': 10}
                                         # search_type="similarity_score_threshold",
                                         # search_kwargs={'score_threshold': 0.8}
                                        )
 
+# Chroma 의 저장된 벡터 DB에 대한 현황 파악 !!!!!!!!!!
+# query = '클라우드 컴퓨팅의 정의는 무엇인가요?'
+# docs = new_docsearch.similarity_search(query, k=3)
+# print('########################################')
+# print(len(docs))
+# print(docs[0].page_content)
 
-'''
-offline_faiss_save 또는 offline_chroma_save 와 같이 pinecone 함수 생성 !!!!!!!!!!!!!!!!!
-'''
+
+##################################################################################
+# offline_faiss_save 또는 offline_chroma_save 와 같이 pinecone 함수 생성 !!!!!!!!!!!!!!!!!
+
 
 
 try:
@@ -181,7 +193,7 @@ class CompletionExecutor(LLM):
             'messages': preset_text,
             'topP': 0.8,
             'topK': 0,
-            'maxTokens': 128,
+            'maxTokens': 256,
             'temperature': 0.1,
             'repeatPenalty': 5.0,
             'stopBefore': [],
@@ -209,34 +221,111 @@ retrieval_qa_chain = ConversationalRetrievalChain.from_llm(llm = hcx_llm,
                                 combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT}
                                 )
 
-class SimpleJsonOutputParser:
-    def __call__(self, result):
-        return result["answer"]
+# class SimpleJsonOutputParser:
+#     def __call__(self, result):
+#         return result["answer"]
 
-retrieval_qa_chain_pipe = retrieval_qa_chain | SimpleJsonOutputParser()
+# retrieval_qa_chain_pipe = retrieval_qa_chain | SimpleJsonOutputParser()
 
-app = FastAPI(title="Cloud Bot",
-        version="1.0",
-        description="A simple API server using LangChain's Runnable interfaces")
+# app = FastAPI(title="Cloud Bot",
+#         version="1.0",
+#         description="A simple API server using LangChain's Runnable interfaces")
 
-# Add the LangServe routes to the FastAPI app
-# 3. Adding chain route
-add_routes(
-    app,
-    retrieval_qa_chain_pipe, 
-    path="/retrieval_qa_chain",
-    )
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# # Add the LangServe routes to the FastAPI app
+# # 3. Adding chain route
+# add_routes(
+#     app,
+#     retrieval_qa_chain_pipe, 
+#     path="/retrieval_qa_chain",
+#     )
 
 
-'''
-/docs: langserve API 문서
-uvicorn langserve_hcx_cloud_bot:app --host 223.130.140.95 --port 8080
-Playground UI
-http://0.0.0.0:8000/retrieval_qa_chain/playground/
-''' 
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+################### LangSerce ###################
+# /docs: langserve API 문서
+# uvicorn langserve_hcx_cloud_bot:app --host 223.130.140.95 --port 8080
+# Playground UI
+# http://0.0.0.0:8000/retrieval_qa_chain/playground/
+
+
+
+st.title("Cloud 관련 무물보~!")
+
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+ 
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+ 
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('질문', '', key='ques')
+
+    submitted = st.form_submit_button('답변')
+ 
+    if submitted and user_input:
+        with st.spinner("Waiting for HyperCLOVA..."): 
+            response_text_json = retrieval_qa_chain({'question': user_input})
+            response_text = response_text_json['answer']
+            
+            # 참조 문서 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            total_content = pd.DataFrame(columns=['순번', '참조 문서'])
+            token_total_content = ''
+            for i in range(len(response_text_json['source_documents'])):
+                context = response_text_json['source_documents'][i].page_content
+                # print('==================================================')
+                # print('\n%d번 째 참조 문서: %s' %(i+1, context))
+                # total_content += '=================================================='
+                # total_content += '\n%d번 째 참조 문서: %s' %(i+1, context)
+                total_content.loc[i] = [i+1, context]
+                                
+            single_turn_text_json = {
+            "messages": [
+            {
+                "role": "system",
+                "content": template
+            },
+            {
+                "role": "system",
+                "content": token_total_content
+            },
+            {
+                "role": "user",
+                "content": user_input
+            },
+            {
+                "role": "assistant",
+                "content": response_text
+            }
+            ]
+            }
+            
+            single_turn_text_token = token_completion_executor.execute(single_turn_text_json)
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            print(single_turn_text_json)
+            # single_turn_token_count = single_turn_text_token[0]['count'] + single_turn_text_token[1]['count'] + single_turn_text_token[2]['count'] + single_turn_text_token[3]['count']
+            single_turn_token_count = sum(token['count'] for token in single_turn_text_token[:4])
+
+            st.session_state.past.append({'question': user_input})
+            # st.session_state.generated.append({'generated': response_text, 'token_count': single_turn_token_count, 'source_documents': total_content})
+            st.session_state.generated.append({'generated': response_text, 'token_count': single_turn_token_count})
+            
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated']) - 1, -1, -1):
+                user_input = st.session_state['past'][i]
+                response = st.session_state['generated'][i]
+
+                message(f"질문: {user_input['question']}", is_user=True, key=str(i) + '_question')
+                message(f"답변: {response['generated']}", is_user=False, key=str(i) + '_generated')
+                # message(f"참조 문서: {response['source_documents']}", is_user=False, key=str(i) + '_source_documents')
+                message(f"총 토큰 수: {response['token_count']}", is_user=False, key=str(i) + '_token_count')
+                st.table(data = total_content)
+
+
+################### Streamlit ###################
+# streamlit run cloud_llm_bot.py
