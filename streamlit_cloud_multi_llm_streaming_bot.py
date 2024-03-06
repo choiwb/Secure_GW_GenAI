731f62983021008d2915b3f5d64ef33bbf6ad5cc
Woobin Choi <khuam1216@gmail.com>
2024-03-06 19:59:29 +0900

    Update streamlit_cloud_multi_llm_streaming_bot.py

--- streamlit_cloud_multi_llm_streaming_bot.py
+++ streamlit_cloud_multi_llm_streaming_bot.py
@@ -5,7 +5,7 @@ import streamlit as st
 # from streamlit_hcx_cloud_bot import cache_instance
 try:
     from streamlit_cloud_llm_bot import hcx_only, hcx_only_2, hcx_general, hcx_stream, retrieval_qa_chain, asa_memory, hcx_memory, gpt_memory, hcx_sec, hcx_sec_pipe, hcx_only_pipe, gpt_pipe
- except Exception as e:
+except Exception as e:
     # 페이지를 자동으로 다시 실행
     st.experimental_rerun()
   
