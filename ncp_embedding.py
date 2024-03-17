d502268a121714d95c5b2eaedd03fecfa8a094b5
Woobin Choi <khuam1216@gmail.com>
2024-03-17 18:28:23 +0900

    commit

---
+++ ncp_embedding.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import os
+import uuid
+from dotenv import load_dotenv
+import json
+import http.client
+
+
+load_dotenv()
+HCX_API_KEY_PRIMARY_VAL=os.getenv("HCX_API_KEY_PRIMARY_VAL")
+REQUEST_ID=str(uuid.uuid4())
+HCX_EMBEDDING_API_KEY=os.getenv("HCX_EMBEDDING_API_KEY")
+
+class HCXEmbedding:
+    # def __init__(self, host, api_key, api_key_primary_val, request_id):
+    #     self._host = host
+    #     self._api_key = API_KEY
+    #     self._api_key_primary_val = api_key_primary_val
+    #     self._request_id = request_id
+    
+    def __init__(self):
+        self.documents = []
+        self.document_embeddings = []
+    
+    def _send_request(self, completion_request):
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-NCP-CLOVASTUDIO-API-KEY': HCX_EMBEDDING_API_KEY,
+            'X-NCP-APIGW-API-KEY': HCX_API_KEY_PRIMARY_VAL,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID
+        }
+
+        conn = http.client.HTTPSConnection(os.getenv("HCX_TOKEN_HOST"))
+        conn.request('POST', '/testapp/v1/api-tools/embedding/clir-emb-dolphin/266736b2551c43ff83fd1d18a524923b', json.dumps(completion_request), headers)
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding='utf-8'))
+        conn.close()
+        return result
+
+    def embed_query(self, completion_request):
+        res = self._send_request(completion_request)
+        if res['status']['code'] == '20000':
+            return res['result']['embedding']
+        else:
+            return 'Error'
+        
+    def embed_documents(self, documents):
+        try:
+            self.documents = documents
+            self.document_embeddings = [self.embed_query({"text": doc}) for doc in documents]
+            return self.document_embeddings
+        except:
+            self.document_embeddings = [self.embed_query({"text": documents})]
+            return self.document_embeddings
+
+
+
+
+if __name__ == '__main__':
+    embeddings = HCXEmbedding()
+
+    request_data = json.loads("""{
+    "text" : "안녕"
+}""", strict=False)
+
+    # response_query = embeddings.embed_query(request_data)
+    # response_text = embeddings.embed_documents(request_data)
+    # print('***************************')
+    # print(response_text)
+    # print('***************************')
+    # print(response_query)
+
+
+
+
---
+++ ncp_vector_db.py
@@ -0,0 +1,118 @@
+
+import os
+import json
+import time
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma, FAISS
+from langchain_community.vectorstores.pgvector import PGVector
+from langchain.text_splitter import CharacterTextSplitter
+from ncp_embedding import  HCXEmbedding
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+ahn_asec_path = 'pdf data path'
+db_save_path = "vector db save folder path" 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+pdf_paths = []
+for filename in os.listdir(ahn_asec_path):
+    if filename.endswith(".pdf"):
+        # 완전한 파일 경로 생성
+        globals()[f'pdf_path_{filename}']  = os.path.join(ahn_asec_path, filename)
+        # print(globals()[f'pdf_path_{filename}'])
+        pdf_paths.append(globals()[f'pdf_path_{filename}'])
+
+embeddings = HCXEmbedding()
+# embeddings = OpenAIEmbeddings(model = 'text-embedding-3-small')
+ 
+text_splitter = CharacterTextSplitter(        
+                            separator = "\n",
+                            chunk_size = 200,
+                            chunk_overlap  = 50,
+                            length_function = len,
+                            )
+
+    
+def offline_chroma_save(pdf_paths):
+
+    total_docs = []
+    total_embed_query = []
+    
+    for pdf_url in pdf_paths:
+        pdfreader =  PyPDFLoader(pdf_url)
+        pdf_doc = pdfreader.load_and_split()
+        doc = text_splitter.split_documents(pdf_doc)
+        total_docs = total_docs + doc
+        
+        page_content = [doc.page_content for doc in doc]
+        
+        for i in range(len(page_content)):
+            doc_json = {"text": page_content[i]}
+            # 1024 차원
+            # single_embed_query = embeddings.embed_query(doc_json)
+            single_embed_query = embeddings.embed_documents(doc_json)
+            total_embed_query.append(single_embed_query)
+
+    print(len(total_embed_query))
+    print(len(total_docs))
+    
+    vectorstore = Chroma.from_documents(
+        documents=total_docs,
+        embedding=embeddings,
+        persist_directory=os.path.join(ROOT_DIR, db_save_path, "cloud_bot_20240317_chroma_db")
+        )
+    vectorstore.persist()
+
+
+
+
+CONNECTION_STRING = "postgresql+psycopg2://ID:PW@localhost:5432/DB NAME"
+COLLECTION_NAME = "pgvector_db"
+'''
+postgresql 설치 후, pgvector 설치해야 함.
+'''
+
+
+def offline_pgvector_save(pdf_paths):
+
+    total_docs = []
+    total_embed_query = []
+    
+    for pdf_url in pdf_paths:
+        pdfreader =  PyPDFLoader(pdf_url)
+        pdf_doc = pdfreader.load_and_split()
+        doc = text_splitter.split_documents(pdf_doc)
+        total_docs = total_docs + doc
+        
+        page_content = [doc.page_content for doc in doc]
+        
+        for i in range(len(page_content)):
+            doc_json = {"text": page_content[i]}
+            # 1024 차원
+            # single_embed_query = embeddings.embed_query(doc_json)
+            single_embed_query = embeddings.embed_documents(doc_json)
+            total_embed_query.append(single_embed_query)
+
+    print(len(total_embed_query))
+    print(len(total_docs))
+            
+    vectorstore = PGVector.from_documents(
+            documents=total_docs,            
+            embedding=embeddings,
+            collection_name=COLLECTION_NAME,
+            connection_string=CONNECTION_STRING,
+        )
+
+ 
+start = time.time()
+total_content = offline_chroma_save(pdf_paths)
+# total_content = offline_pgvector_save(pdf_paths)
+end = time.time()
+'''임베딩 완료 시간: 1.31 (초)'''
+print('임베딩 완료 시간: %.2f (초)' %(end-start))
