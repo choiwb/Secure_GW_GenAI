959b2b63cdda53ef1e5f91fb5dc36d855e71f0f3
Woobin Choi <khuam1216@gmail.com>
2024-04-06 15:08:29 +0900

    commit

---
+++ LCEL.py
@@ -0,0 +1,157 @@
+
+
+import os
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+import streamlit as st
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from operator import itemgetter
+from langchain_core.messages import get_buffer_string
+from langchain.schema import format_document
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain.vectorstores import Chroma
+
+from config import db_save_path, DB_COLLECTION_NAME, DB_CONNECTION_STRING
+from vector_db import embeddings
+from prompt import not_rag_template, rag_template
+from LLM import HCX_sec, HCX_only, HCX_stream, gpt_model, gemini_vis_model, gemini_txt_model
+
+
+ 
+ONLY_CHAIN_PROMPT = PromptTemplate(input_variables=["question"],template=not_rag_template)
+SEC_CHAIN_PROMPT = PromptTemplate(input_variables=["question"],template=not_rag_template)
+QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=rag_template)
+ 
+hcx_sec = HCX_sec()
+hcx_stream = HCX_stream()
+hcx_only = HCX_only() 
+ 
+new_docsearch = Chroma(persist_directory=os.path.join(db_save_path, "cloud_assistant_v1"),
+                        embedding_function=embeddings)
+
+# new_docsearch = PGVector(collection_name=DB_COLLECTION_NAME, connection_string=DB_CONNECTION_STRING,
+#                         embedding_function=embeddings)
+
+retriever = new_docsearch.as_retriever(
+                                        search_type="mmr",                                        
+                                        search_kwargs={'k': 8, 'fetch_k': 32}
+                                       )
+
+
+# retriever의 compression 시도 !!!!!!!!!!!!!!!!!!!!!!!!!
+'''
+ncp embedding의 경우
+ValidationError: 1 validation error for EmbeddingsFilter embeddings instance of Embeddings expected (type=type_error.arbitrary_type; expected_arbitrary_type=Embeddings)
+'''
+embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.3) 
+
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=embeddings_filter, base_retriever=retriever
+)
+ 
+@st.cache_resource
+def asa_init_memory():
+    return ConversationBufferMemory(
+        input_key='question',
+        output_key='answer',
+        memory_key='chat_history',
+        return_messages=True)
+asa_memory = asa_init_memory()
+ 
+@st.cache_resource
+def hcx_init_memory():
+    return ConversationBufferMemory(
+        input_key='question',
+        output_key='answer',
+        memory_key='chat_history',
+        return_messages=True)
+hcx_memory = hcx_init_memory()
+ 
+@st.cache_resource
+def gpt_init_memory():
+    return ConversationBufferMemory(
+        input_key='question',
+        output_key='answer',
+        memory_key='chat_history',
+        return_messages=True)
+gpt_memory = gpt_init_memory()
+ 
+@st.cache_resource
+def gemini_init_memory():
+    return ConversationBufferMemory(
+        input_key='question',
+        output_key='answer',
+        memory_key='chat_history',
+        return_messages=True)
+gemini_memory = gemini_init_memory()
+ 
+# reset button
+def reset_conversation():
+  print('대화 리셋!')
+  st.session_state.conversation = None
+  st.session_state.chat_history = None
+  asa_memory.clear()
+  hcx_memory.clear()
+  gpt_memory.clear()
+  gemini_memory.clear()
+  
+  
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+  
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+ 
+
+asa_loaded_memory = RunnablePassthrough.assign(
+    chat_history=RunnableLambda(asa_memory.load_memory_variables) | itemgetter("chat_history"),
+)
+ 
+hcx_loaded_memory = RunnablePassthrough.assign(
+    chat_history=RunnableLambda(hcx_memory.load_memory_variables) | itemgetter("chat_history"),
+)
+ 
+gpt_loaded_memory = RunnablePassthrough.assign(
+    chat_history=RunnableLambda(gpt_memory.load_memory_variables) | itemgetter("chat_history"),
+)
+
+gemini_loaded_memory = RunnablePassthrough.assign(
+    chat_history=RunnableLambda(gemini_memory.load_memory_variables) | itemgetter("chat_history"),
+)
+  
+retrieved_documents = {
+    "question": lambda x: x["question"],
+    "source_documents": itemgetter("question") | compression_retriever,
+    "chat_history": lambda x: get_buffer_string(x["chat_history"])
+}
+ 
+not_retrieved_documents = {
+    "question": lambda x: x["question"],
+    "chat_history": lambda x: get_buffer_string(x["chat_history"])
+}
+
+img_retrieved_documents = {
+    "context": lambda x: x["context"],
+    "question": lambda x: x["question"]
+}
+ 
+final_inputs = {
+    "context": lambda x: _combine_documents(x["source_documents"]),
+    "question": itemgetter("question"),
+    "chat_history": itemgetter("chat_history")
+}
+
+
+hcx_sec_pipe = SEC_CHAIN_PROMPT | hcx_sec | StrOutputParser()
+retrieval_qa_chain =  asa_loaded_memory | retrieved_documents | final_inputs | QA_CHAIN_PROMPT | hcx_stream | StrOutputParser()
+hcx_only_pipe =  hcx_loaded_memory | not_retrieved_documents | ONLY_CHAIN_PROMPT | hcx_only | StrOutputParser()
+gpt_pipe =  gpt_loaded_memory | not_retrieved_documents | ONLY_CHAIN_PROMPT | gpt_model | StrOutputParser()
+
+gemini_txt_pipe = gemini_loaded_memory | not_retrieved_documents | ONLY_CHAIN_PROMPT | gemini_txt_model | StrOutputParser()
+gemini_vis_pipe = RunnablePassthrough() | gemini_vis_model | StrOutputParser()
+gemini_vis_txt_pipe = gemini_loaded_memory | img_retrieved_documents | QA_CHAIN_PROMPT | gemini_txt_model | StrOutputParser()
---
+++ LLM.py
@@ -0,0 +1,245 @@
+
+
+import os
+import uuid
+import random
+import json
+import httpx
+from dotenv import load_dotenv
+import requests
+import pandas as pd
+from typing import Any, List, Optional
+from langchain.llms.base import LLM
+import streamlit as st
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.chat_models import ChatOpenAI  
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from hcx_token_cal import token_completion_executor
+from prompt import PROMPT_INJECTION_PROMPT, SYSTEMPROMPT
+
+
+##################################################################################
+# .env 파일 로드
+load_dotenv()
+
+API_KEY=os.getenv('HCX_API_KEY')
+API_KEY_PRIMARY_VAL=os.getenv('HCX_API_KEY_PRIMARY_VAL')
+REQUEST_ID=str(uuid.uuid4())
+
+os.getenv('OPENAI_API_KEY')
+
+# HCX LLM 경로 !!!!!!!!!!!!!!!!!!!!!!!
+llm_url = os.getenv('HCX_LLM_URL')
+
+# Set Google API key
+os.getenv("GOOGLE_API_KEY")
+os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+##################################################################################
+
+def hcx_stream_process(res):
+    full_response = ""
+    message_placeholder = st.empty()       
+
+    for line in res.iter_lines():
+        if line.startswith("data:"):
+            split_line = line.split("data:")
+            line_json = json.loads(split_line[1])
+            if "stopReason" in line_json and line_json["stopReason"] == None:
+                full_response += line_json["message"]["content"]
+                message_placeholder.markdown(full_response, unsafe_allow_html=True)
+    return full_response
+
+class HCX_sec(LLM):        
+   
+    init_input_token_count: int = 0
+
+    @property
+    def _llm_type(self) -> str:
+        return "hcx-003"
+   
+    def _call(
+        self,
+        prompt: str,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+       
+        preset_text = [{"role": "system", "content": PROMPT_INJECTION_PROMPT}, {"role": "user", "content": prompt}]
+       
+        output_token_json = {
+            "messages": preset_text
+            }
+       
+        total_input_token_json = token_completion_executor.execute(output_token_json)
+        self.init_input_token_count = sum(token['count'] for token in total_input_token_json[:])
+               
+        request_data = {
+        'messages': preset_text,
+        'topP': 0.8,
+        'topK': 0,
+        'maxTokens': 128,
+        'temperature': 0.1,
+        'repeatPenalty': 5.0,
+        'stopBefore': [],
+        'includeAiFilters': True,
+        "seed": 4595
+        }
+       
+       
+        general_headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': API_KEY,
+            'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+            'Content-Type': 'application/json; charset=utf-8',
+        }
+               
+        response = requests.post(llm_url, json=request_data, headers=general_headers, verify=False)
+        response.raise_for_status()
+        
+        llm_result = response.json()['result']['message']['content']
+        
+        preset_text = [{"role": "system", "content": ""}, {"role": "user", "content": llm_result}]
+        
+        output_token_json = {
+            "messages": preset_text
+            }
+       
+        total_input_token_json = token_completion_executor.execute(output_token_json)
+        self.init_input_token_count += sum(token['count'] for token in total_input_token_json[:])
+                          
+        return llm_result
+       
+ 
+class HCX_stream(LLM):      
+   
+    init_input_token_count: int = 0
+    source_documents: str = ""
+    sample_src_doc_df = pd.DataFrame()
+
+    @property
+    def _llm_type(self) -> str:
+        return "hcx-003"
+   
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        if stop is not None:
+            raise ValueError("stop kwargs are not permitted.")
+       
+        preset_text = [{"role": "system", "content": SYSTEMPROMPT}, {"role": "user", "content": prompt}]
+               
+        # prompt 변수의 context for answer: 부터 question: 이전 text를 source_documents 선언
+        self.source_documents = prompt.split("context for answer: ")[1].split("question: ")[0]
+        if len(self.source_documents.strip()) > 0:
+            source_documents_list = self.source_documents.split('\n\n')
+            sample_src_doc = random.sample(source_documents_list, 2)
+            sample_src_doc = [[i+1, doc[:100] + '.....(이하 생략)'] for i, doc in enumerate(sample_src_doc)] 
+            self.sample_src_doc_df = pd.DataFrame(sample_src_doc,  columns=['No', '참조 문서'])
+            self.sample_src_doc_df = self.sample_src_doc_df.set_index('No')
+
+        output_token_json = {
+            "messages": preset_text
+            }
+       
+        total_input_token_json = token_completion_executor.execute(output_token_json)
+        self.init_input_token_count = sum(token['count'] for token in total_input_token_json[:])
+       
+        request_data = {
+        'messages': preset_text,
+        'topP': 0.8,
+        'topK': 0,
+        'maxTokens': 512,
+        'temperature': 0.1,
+        'repeatPenalty': 5.0,
+        'stopBefore': [],
+        'includeAiFilters': True,
+        "seed": 4595
+        }
+                       
+        stream_headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': API_KEY,
+            'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+            'Content-Type': 'application/json; charset=utf-8',
+            # streaming 옵션 !!!!!
+            'Accept': 'text/event-stream'
+        }
+        
+        with httpx.stream(method="POST",
+                        url=llm_url,
+                        json=request_data,
+                        headers=stream_headers,
+                        timeout=10) as res:
+            full_response = hcx_stream_process(res)
+            return full_response
+        
+class HCX_only(LLM):        
+    
+    init_input_token_count: int = 0
+ 
+    @property
+    def _llm_type(self) -> str:
+        return "hcx-003"
+   
+    def _call(
+        self,
+        prompt: str,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+       
+        preset_text = [{"role": "system", "content": ""}, {"role": "user", "content": prompt}]
+       
+        output_token_json = {
+            "messages": preset_text
+            }
+       
+        total_input_token_json = token_completion_executor.execute(output_token_json)
+        self.init_input_token_count = sum(token['count'] for token in total_input_token_json[:])
+       
+        request_data = {
+        'messages': preset_text,
+        'topP': 0.8,
+        'topK': 0,
+        'maxTokens': 512,
+        'temperature': 0.1,
+        'repeatPenalty': 5.0,
+        'stopBefore': [],
+        'includeAiFilters': True,
+        "seed": 4595
+        }
+                       
+        stream_headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': API_KEY,
+            'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+            'Content-Type': 'application/json; charset=utf-8',
+            # streaming 옵션 !!!!!
+            'Accept': 'text/event-stream'
+        }
+        
+        with httpx.stream(method="POST",
+                        url=llm_url,
+                        json=request_data,
+                        headers=stream_headers,
+                        timeout=10) as res:
+            full_response = hcx_stream_process(res)
+            return full_response
+        
+        
+gpt_model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    # GPT-4 Turbo 
+    # model="gpt-4-0125-preview",
+
+    max_tokens=512,
+    temperature=0.1
+)    
+
+gemini_txt_model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.1, max_output_tokens=512)
+gemini_vis_model = ChatGoogleGenerativeAI(model="gemini-pro-vision", temperature=0.1, max_output_tokens=512)
