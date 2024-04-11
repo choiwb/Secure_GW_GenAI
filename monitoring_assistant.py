7c08f2e877f3fd72cf3138ddbead80dc8543e5b3
Woobin Choi <khuam1216@gmail.com>
2024-04-11 12:53:20 +0900

    Update monitoring_assistant.py

--- monitoring_assistant.py
+++ monitoring_assistant.py
@@ -45,13 +45,32 @@ with st.expander('추천 질문'):
 with st.expander('Protocol Stack'):
     st.image(asa_image_path, caption='Protocol Stack', use_column_width=True)
 
-def scroll_bottom() -> None:
-    js = '''
+def scroll_bottom():
+    js = f"""
     <script>
-        window.scrollTo(0, document.body.scrollHeight);
+        // 스크롤을 하단으로 이동시키는 함수
+        function scrollToBottom(){{
+            var textAreas = parent.document.querySelectorAll('section.main');
+            for (let index = 0; index < textAreas.length; index++) {{
+                textAreas[index].scrollTop = textAreas[index].scrollHeight;
+            }}
+        }}
+
+        // MutationObserver의 콜백 함수 정의
+        function observeMutations(){{
+            var observer = new MutationObserver(scrollToBottom);
+            var config = {{ childList: true, subtree: true }};
+            // 감시 대상 요소 지정 및 옵저버 시작
+            var target = parent.document.querySelector('section.main');
+            if(target) observer.observe(target, config);
+        }}
+
+        // 초기 스크롤 위치 조정 및 DOM 변화 감지를 위한 옵저버 설정
+        scrollToBottom();
+        observeMutations();
     </script>
-    '''
-    st.components.v1.html(js, width=0, height=0)
+    """
+    st.components.v1.html(js, height=0) 
     
 if "rerun_tab" not in st.session_state:
     reset_conversation()
