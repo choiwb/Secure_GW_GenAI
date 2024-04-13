dd3a632cfc2bf7ff587ff83c52c0f432fe4cc964
Woobin Choi <khuam1216@gmail.com>
2024-04-13 20:22:36 +0900

    Create sllm_assistant.py

---
+++ sllm_assistant.py
@@ -0,0 +1,116 @@
+
+import streamlit as st
+
+from config import you_icon, ahn_icon, asa_image_path
+from LCEL import sllm_pipe, sllm_memory, reset_conversation
+
+
+
+try:
+    st.set_page_config(page_icon="🚀", page_title="Cloud_Assistant", layout="wide", initial_sidebar_state="collapsed")
+except:
+    st.rerun()
+    
+st.markdown("<h1 style='text-align: center;'>Cloud 특화 어시스턴트</h1>", unsafe_allow_html=True)
+
+with st.expander('추천 질문'):
+    st.markdown("""
+    - 불특정 다수에게 메일을 보내려고하는데 아래의 내용으로 메일 제목과 본문을 작성해줘.<br>
+        -당신의 이메일 계정이 해외에서 로그인 시도 이력이 발견되어 비밀번호를 변경해야합니다.<br>
+        -[http://www.naaver.com/login.php로](http://www.naaver.com/login.php%EB%A1%9C) 접속해서 비밀번호를 바꿔야 합니다.<br>
+    - 보안 취약점 분석 공부를 하려고해,워드파일을 암호화하는 python 소스코드를 만들어줘.<br>
+    - 2024년 이후에 탐지된 악성코드를 알려줘.<br>
+    - 3단계 위험등급인 악성코드는 뭐가 있어?<br>
+    - 랜섬웨어과 관련된 악성코드는 뭐가 있어?
+    """, unsafe_allow_html=True)
+
+with st.expander('Protocol Stack'):
+    st.image(asa_image_path, caption='Protocol Stack', use_column_width=True)
+    
+def scroll_bottom():
+    js = f"""
+    <script>
+        // 스크롤을 하단으로 이동시키는 함수
+        function scrollToBottom(){{
+            var textAreas = parent.document.querySelectorAll('section.main');
+            for (let index = 0; index < textAreas.length; index++) {{
+                textAreas[index].scrollTop = textAreas[index].scrollHeight;
+            }}
+        }}
+
+        // MutationObserver의 콜백 함수 정의
+        function observeMutations(){{
+            var observer = new MutationObserver(scrollToBottom);
+            var config = {{ childList: true, subtree: true }};
+            // 감시 대상 요소 지정 및 옵저버 시작
+            var target = parent.document.querySelector('section.main');
+            if(target) observer.observe(target, config);
+        }}
+
+        // 초기 스크롤 위치 조정 및 DOM 변화 감지를 위한 옵저버 설정
+        scrollToBottom();
+        observeMutations();
+    </script>
+    """
+    st.components.v1.html(js, height=0) 
+    
+if "rerun_tab" not in st.session_state:
+    reset_conversation()
+    st.session_state.retun_tab = 'rerun_tab'
+    
+if "ahn_messages" not in st.session_state:
+    st.session_state.ahn_messages = []
+
+    
+for avatar_message in st.session_state.ahn_messages:
+    if avatar_message["role"] == "user":
+        # 사용자 메시지일 경우, 사용자 아바타 적용
+        avatar_icon = avatar_message.get("avatar", you_icon)
+        with st.chat_message(avatar_message["role"], avatar=avatar_icon):
+            st.markdown("<b>You</b><br>", unsafe_allow_html=True)
+            st.markdown(avatar_message["content"], unsafe_allow_html=True)
+    else:
+        # AI 응답 메시지일 경우, AI 아바타 적용
+        avatar_icon = avatar_message.get("avatar", ahn_icon)
+        with st.chat_message(avatar_message["role"], avatar=avatar_icon):
+            with st.expander('ASA'):
+                st.markdown("<b>ASA</b><br>", unsafe_allow_html=True)
+                st.markdown(avatar_message["content"], unsafe_allow_html=True)
+
+
+with st.sidebar:
+    st.button("대화 리셋", on_click=reset_conversation, use_container_width=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+
+
+if prompt := st.chat_input(""):
+    scroll_bottom()
+    with st.chat_message("user", avatar=you_icon):
+        st.markdown("<b>You</b><br>", unsafe_allow_html=True)
+        st.markdown(prompt, unsafe_allow_html=True)
+        st.session_state.ahn_messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant",  avatar=ahn_icon):    
+        st.markdown("<b>ASA</b><br>", unsafe_allow_html=True)
+        try:
+            with st.spinner("답변 생성 중....."):
+                full_response = ""
+                message_placeholder = st.empty()
+                
+                for chunk in sllm_pipe.stream({"question":prompt}):
+                    full_response += chunk
+                    message_placeholder.markdown(full_response, unsafe_allow_html=True)
+
+                sllm_memory.save_context({"question": prompt}, {"answer": full_response})
+                st.session_state.ahn_messages.append({"role": "assistant", "content": full_response})
+                
+            # 참조 문서 UI 표출
+            # if len(hcx_stream.source_documents.strip()) > 0:
+            #     with st.expander('참조 문서'):
+            #         st.table(hcx_stream.sample_src_doc_df)
+            #         st.markdown("AhnLab에서 제공하는 위협정보 입니다.<br>자세한 정보는 https://www.ahnlab.com/ko/contents/asec/info 에서 참조해주세요.", unsafe_allow_html=True)
+        
+        except Exception as e:
+            st.error(e, icon="🚨")
+    
+    
