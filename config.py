95dbd9b8bd6bc095ccb1d89a2aee1a0124001c23
Woobin Choi <khuam1216@gmail.com>
2024-04-15 23:13:31 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
 
 import os
+import uuid
+from dotenv import load_dotenv
+
+##################################################################################
+# .env 파일 로드
+load_dotenv()
+
+API_KEY=os.getenv('HCX_API_KEY')
+API_KEY_PRIMARY_VAL=os.getenv('HCX_API_KEY_PRIMARY_VAL')
+REQUEST_ID=str(uuid.uuid4())
+
+TOKEN_API_KEY=os.getenv('HCX_TOKEN_API_KEY')
+##################################################################################
 
 # pdf 파일 저장 경로
 pdf_folder_path = os.path.join(os.getcwd(), 'YOUR PATH !!!!!!!')
@@ -25,4 +38,35 @@ sllm_model_path = os.path.join(os.getcwd(), "sllm_models/EEVE-Korean-Instruct-10
 sllm_embed_model_path = os.path.join(os.getcwd(), "sllm_models/nomic-embed-text-v1.5.f32.gguf")
 sllm_n_gpu_layers = 1  # Metal set to 1 is enough.
 sllm_n_batch = 8192  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
+
+token_headers = {
+        'X-NCP-CLOVASTUDIO-API-KEY': TOKEN_API_KEY,
+        'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+        'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+        'Content-Type': 'application/json; charset=utf-8',
+        }
+
+hcx_general_headers = {
+        'X-NCP-CLOVASTUDIO-API-KEY': API_KEY,
+        'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+        'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+        'Content-Type': 'application/json; charset=utf-8',
+        }
+
+hcx_stream_header = {
+        'X-NCP-CLOVASTUDIO-API-KEY': API_KEY,
+        'X-NCP-APIGW-API-KEY': API_KEY_PRIMARY_VAL,
+        'X-NCP-CLOVASTUDIO-REQUEST-ID': REQUEST_ID,
+        'Content-Type': 'application/json; charset=utf-8',
+        # streaming 옵션 !!!!!
+        'Accept': 'text/event-stream'
+        }
+
+sec_headers = {
+        # 응답에 보안 헤더 추가
+        'Strict-Transport-Security': 'max-age=63072000; includeSubdomains; preload',
+        'X-Content-Type-Options': 'nosniff',
+        'Content-Security-Policy': "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; frame-ancestors 'none'",
+        'referrer-policy': 'same-origin'
+        }
 
