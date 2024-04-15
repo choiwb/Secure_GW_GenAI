84abb3814afc184eaac076c247b49e5c971672df
Woobin Choi <khuam1216@gmail.com>
2024-04-15 23:09:57 +0900

    Update multi_llm_assistant.py

--- multi_llm_assistant.py
+++ multi_llm_assistant.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from config import you_icon, hcx_icon, ahn_icon, gpt_icon, asa_image_path
 from LCEL import hcx_only, hcx_stream, retrieval_qa_chain, asa_memory, hcx_memory, gpt_memory, hcx_sec, hcx_sec_pipe, hcx_only_pipe, gpt_pipe, reset_conversation
-from hcx_token_cal import token_completion_executor
+from LLM import token_completion_executor
 
 
 
