16501ce417670719e91802181a661d37b952e690
Woobin Choi <khuam1216@gmail.com>
2024-04-27 15:32:47 +0900

    Update vector_db.py

--- vector_db.py
+++ vector_db.py
@@ -22,7 +22,7 @@ text_splitter = RecursiveCharacterTextSplitter(
     )
 
 # 오프라인 데이터 가공 ####################################################################################
-# OpenAI 또는 HuggingFace Embedding
+# OpenAI, NCP, HuggingFace Embedding
 def offline_chroma_save(pdf_paths):
  
     total_docs = []
@@ -36,67 +36,26 @@ def offline_chroma_save(pdf_paths):
     vectorstore = Chroma.from_documents(
         documents=total_docs,
         embedding=embeddings,
-        persist_directory=os.path.join(db_save_path, "cloud_assistant_v4")
-        )
-    vectorstore.persist()
-
-
-# NCP Embedding
-def ncp_offline_chroma_save(pdf_paths):
-
-    total_docs = []
-    total_embed_query = []
-    
-    for pdf_url in pdf_paths:
-        pdfreader =  PyPDFLoader(pdf_url)
-        pdf_doc = pdfreader.load_and_split()
-        doc = text_splitter.split_documents(pdf_doc)
-        total_docs = total_docs + doc
-
-        page_content = [doc.page_content for doc in doc]
-
-        for content in page_content:
-            single_embed_query = embeddings.embed_query(content)
-            # 1024 차원
-            # print(len(single_embed_query))
-            total_embed_query.append(single_embed_query)
-    
-    vectorstore = Chroma.from_documents(
-        documents=total_docs,
-        embedding=embeddings,
         persist_directory=os.path.join(db_save_path, "cloud_assistant_v3")
         )
     vectorstore.persist()
 
-'''
-postgresql 설치 후, pgvector 설치해야 함.
-'''
+
+# postgresql 설치 후, pgvector 설치해야 함.
 def offline_pgvector_save(pdf_paths):
 
     total_docs = []
-    total_embed_query = []
     
     for pdf_url in pdf_paths:
         pdfreader =  PyPDFLoader(pdf_url)
         pdf_doc = pdfreader.load_and_split()
         doc = text_splitter.split_documents(pdf_doc)
         total_docs = total_docs + doc
-        
-        page_content = [doc.page_content for doc in doc]
-        
-        for content in page_content:
-            single_embed_query = embeddings.embed_query(content)
-            # 1024 차원
-            # print(len(single_embed_query))
-            total_embed_query.append(single_embed_query)
-
-    print(len(total_embed_query))
-    print(len(total_docs))
-            
+                            
     vectorstore = PGVector.from_documents(
             documents=total_docs,            
             embedding=embeddings,
             collection_name=DB_COLLECTION_NAME,
             connection_string=DB_CONNECTION_STRING,
         )
- #######################################################################################################
+#######################################################################################################
