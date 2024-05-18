987e02ed396b86a67d5ca0a33bb7c0e11eff30f8
Woobin Choi <khuam1216@gmail.com>
2024-05-18 18:45:44 +0900

    Update vector_db_update.py

--- vector_db_update.py
+++ vector_db_update.py
@@ -3,19 +3,18 @@ import os
 import time
 
 from vector_db import offline_chroma_save, offline_pgvector_save
-from config import pdf_folder_path
+from config import pdf_folder_path, db_name
 
 
 pdf_paths = []
 for filename in os.listdir(pdf_folder_path):
     if filename.endswith(".pdf"):
         # 완전한 파일 경로 생성
-        globals()[f'pdf_path_{filename}']  = os.path.join(pdf_folder_path, filename)
-        # print(globals()[f'pdf_path_{filename}'])
-        pdf_paths.append(globals()[f'pdf_path_{filename}'])
+        pdf_path = os.path.join(pdf_folder_path, filename)
+        pdf_paths.append(pdf_path)
 
 start = time.time()
-total_content = offline_chroma_save(pdf_paths)
+total_content = offline_chroma_save(pdf_paths, db_name)
 # total_content = offline_pgvector_save(pdf_paths)
 end = time.time()
 print('임베딩 완료 시간: %.2f (초)' %(end-start))
