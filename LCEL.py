ea2a64733f9a41bc3006b960798a42c9effbbcf8
Woobin Choi <khuam1216@gmail.com>
2024-06-09 19:00:36 +0900

    Update LCEL.py

--- LCEL.py
+++ LCEL.py
@@ -1,6 +1,5 @@
 
-
-import os
+import pandas as pd
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 import streamlit as st
@@ -9,15 +8,11 @@ from langchain_core.output_parsers import StrOutputParser
 from operator import itemgetter
 from langchain_core.messages import get_buffer_string
 from langchain.schema import format_document
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import EmbeddingsFilter
-from langchain_community.vectorstores import Chroma
 
-from config import db_save_path, db_name, user_db_name, DB_COLLECTION_NAME, DB_CONNECTION_STRING, db_search_type, db_doc_k, db_doc_fetch_k, db_similarity_threshold
-from vector_db import embeddings
+from config import compression_retriever, user_compression_retriever
 from prompt import not_rag_template, rag_template, img_rag_template, PROMPT_INJECTION_PROMPT, SYSTEMPROMPT
 from LLM import HCX, gpt_model, sonnet_llm, sllm, gemini_vis_model, gemini_txt_model
-from streamlit_custom_func import src_doc
+
 
 ONLY_CHAIN_PROMPT = PromptTemplate(input_variables=["question"],template=not_rag_template)
 QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=rag_template)
@@ -25,28 +20,6 @@ IMG_QA_CHAIN_PROMPT = PromptTemplate(input_variables=["img_context", "context", 
  
 hcx_sec = HCX(init_system_prompt = PROMPT_INJECTION_PROMPT, streaming = False)
 hcx_stream = HCX(init_system_prompt = SYSTEMPROMPT, streaming = True)
-
-# new_docsearch = PGVector(collection_name=DB_COLLECTION_NAME, connection_string=DB_CONNECTION_STRING,
-#                         embedding_function=embeddings)
-
-new_docsearch = Chroma(persist_directory=os.path.join(db_save_path, db_name),
-                            embedding_function=embeddings)
-user_new_docsearch = Chroma(persist_directory=os.path.join(db_save_path, user_db_name),
-                            embedding_function=embeddings)
-
-def retriever_alog(new_docsearch):
-    retriever = new_docsearch.as_retriever(
-                                        search_type=db_search_type,         
-                                        search_kwargs={'k': db_doc_k, 'fetch_k': db_doc_fetch_k}
-                                    )
-    embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=db_similarity_threshold) 
-    compression_retriever = ContextualCompressionRetriever(
-        base_compressor=embeddings_filter, base_retriever=retriever
-    )
-    return compression_retriever
-
-compression_retriever = retriever_alog(new_docsearch)
-user_compression_retriever = retriever_alog(user_new_docsearch)
 
 @st.cache_resource
 def asa_init_memory():
@@ -111,7 +84,7 @@ def _combine_documents(
 ):
     doc_strings = [format_document(doc, document_prompt) for doc in docs]
     return document_separator.join(doc_strings)
-  
+
 asa_loaded_memory = RunnablePassthrough.assign(
     chat_history=RunnableLambda(asa_memory.load_memory_variables) | itemgetter("chat_history"),
 )
@@ -131,13 +104,38 @@ sllm_loaded_memory = RunnablePassthrough.assign(
 gemini_loaded_memory = RunnablePassthrough.assign(
     chat_history=RunnableLambda(gemini_memory.load_memory_variables) | itemgetter("chat_history"),
 )
-   
+
 retrieved_documents = {
     "question": lambda x: x["question"],
     "source_documents": itemgetter("question") | compression_retriever,
     "chat_history": lambda x: get_buffer_string(x["chat_history"])
     }
 
+def src_doc(retrieved_documents):
+
+    if len(retrieved_documents) > 0:
+        source_documents_total = retrieved_documents['source_documents']
+        src_doc_context = [doc.page_content for doc in source_documents_total]
+        src_doc_score = [doc.state['query_similarity_score'] for doc in source_documents_total]
+        src_doc_metadata = [doc.metadata for doc in source_documents_total]
+
+        formatted_metadata = [
+            f'문서 명: {metadata["source"].split("/")[-1]}, 문서 위치: {metadata["page"]} 쪽'
+            for metadata in src_doc_metadata
+        ]
+        
+        src_doc_df = pd.DataFrame({'참조 문서': src_doc_context, '유사도': src_doc_score, '문서 출처': formatted_metadata})
+        src_doc_df['No'] = [i+1 for i in range(src_doc_df.shape[0])]
+        src_doc_df = src_doc_df.set_index('No')
+        src_doc_df['참조 문서'] = src_doc_df['참조 문서'].str.slice(0, 100) + '.....(이하 생략)'
+        src_doc_df['유사도'] = src_doc_df['유사도'].round(3).astype(str)
+        
+        with st.expander('참조 문서'):
+            st.table(src_doc_df)
+            st.markdown("AhnLab에서 제공하는 위협정보 입니다.<br>자세한 정보는 https://www.ahnlab.com/ko/contents/asec/info 에서 참조해주세요.", unsafe_allow_html=True)
+
+    return retrieved_documents
+    
 user_retrieved_documents = {
     "question": lambda x: x["question"],
     "source_documents": itemgetter("question") | user_compression_retriever,
@@ -170,7 +168,7 @@ img_final_inputs = {
 }
 
 def RAG_pipeline(memory, documents, llm):
-    return memory | documents | final_inputs | QA_CHAIN_PROMPT | src_doc | llm | StrOutputParser() 
+    return memory | documents | src_doc | final_inputs | QA_CHAIN_PROMPT | llm | StrOutputParser() 
 
 def not_RAG_pipeline(memory, llm):
     return memory | not_retrieved_documents | ONLY_CHAIN_PROMPT | llm | StrOutputParser()
@@ -186,6 +184,6 @@ sllm_pipe = RAG_pipeline(sllm_loaded_memory, retrieved_documents, sllm)
 gemini_txt_pipe = not_RAG_pipeline(gemini_loaded_memory, gemini_txt_model)
 gemini_vis_pipe = RunnablePassthrough() | gemini_vis_model | StrOutputParser()
 gemini_vis_vectordb_txt_pipe = (
-                                gemini_loaded_memory | img_retrieved_documents | img_final_inputs | 
-                                IMG_QA_CHAIN_PROMPT | src_doc | gemini_txt_model | StrOutputParser()
+                                gemini_loaded_memory | img_retrieved_documents | src_doc | img_final_inputs | 
+                                IMG_QA_CHAIN_PROMPT | gemini_txt_model | StrOutputParser()
                                )
