9114366f63292c0d19ef03dae37fc4ce48b6ed17
Woobin Choi <khuam1216@gmail.com>
2024-06-09 15:04:55 +0900

    Update streamlit_custom_func.py

--- streamlit_custom_func.py
+++ streamlit_custom_func.py
@@ -1,8 +1,15 @@
 
 
+import os
 import json
 import pandas as pd
 import streamlit as st
+from langchain_community.vectorstores import Chroma
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+
+from config import db_name, user_db_name, db_save_path, db_similarity_threshold, db_doc_k, db_search_type, db_doc_fetch_k
+from vector_db import embeddings
 
 
 def hcx_stream_process(res):
@@ -48,13 +55,36 @@ def scroll_bottom():
 def src_doc(prompt):
     prompt_str = str(prompt)
     source_documents = prompt_str.split("context for answer: ")[1].split("question: ")[0]
+    extraction_question = prompt_str.split("question: ")[1]
     
     if len(source_documents.strip()) > 0 and source_documents.strip() != '\\n':
         source_documents_list = source_documents.split('\\n\\n')
         sample_src_doc = [[i+1, doc[:100] + '.....(이하 생략)'] for i, doc in enumerate(source_documents_list)] 
         sample_src_doc_df = pd.DataFrame(sample_src_doc,  columns=['No', '참조 문서'])
         sample_src_doc_df = sample_src_doc_df.set_index('No')
+
+        if st.session_state.selected_db == 'user_vectordb':
+            load_db = Chroma(persist_directory=os.path.join(db_save_path, user_db_name),
+                                embedding_function=embeddings) 
+        else:
+            load_db = Chroma(persist_directory=os.path.join(db_save_path, db_name),
+                                embedding_function=embeddings) 
         
+        retriever = load_db.as_retriever(
+                                                search_type=db_search_type,         
+                                                search_kwargs={'k': db_doc_k, 'fetch_k': db_doc_fetch_k}
+                                            )
+        embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=db_similarity_threshold) 
+        compression_retriever = ContextualCompressionRetriever(
+                base_compressor=embeddings_filter, base_retriever=retriever
+            )
+
+        extraction_context = compression_retriever.invoke(extraction_question)
+        src_doc_score = [doc.state['query_similarity_score'] for doc in extraction_context]
+
+        sample_src_doc_df['유사도'] = src_doc_score
+        sample_src_doc_df['유사도'] = sample_src_doc_df['유사도'].round(2).astype(str)
+                
         # 참조 문서 UI 표출
         if sample_src_doc_df.shape[0] > 0:
             with st.expander('참조 문서'):
