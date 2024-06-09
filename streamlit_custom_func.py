74b321ef42f789ccf9320de4520129639947b618
Woobin Choi <khuam1216@gmail.com>
2024-06-09 18:58:42 +0900

    Update streamlit_custom_func.py

--- streamlit_custom_func.py
+++ streamlit_custom_func.py
@@ -1,11 +1,7 @@
 
 
-import time
 import json
-import pandas as pd
 import streamlit as st
-
-from config import compression_retriever, user_compression_retriever
 
 
 def hcx_stream_process(res):
@@ -47,40 +43,3 @@ def scroll_bottom():
     </script>
     """
     st.components.v1.html(js, height=0)
-    
-def src_doc(prompt):
-    prompt_str = str(prompt)
-    source_documents = prompt_str.split("context for answer: ")[1].split("question: ")[0]
-    extraction_question = prompt_str.split("question: ")[1]
-    
-    if len(source_documents.strip()) > 0 and source_documents.strip() != '\\n':
-        source_documents_list = source_documents.split('\\n\\n')
-        sample_src_doc = [[i+1, doc[:100] + '.....(이하 생략)'] for i, doc in enumerate(source_documents_list)] 
-        sample_src_doc_df = pd.DataFrame(sample_src_doc,  columns=['No', '참조 문서'])
-        sample_src_doc_df = sample_src_doc_df.set_index('No')
-
-        # 문서 추출 소요 시간: 3.16 (초) => 불필요하므로 제거하는것도 고민해 봐야 함!!!!!!
-        start = time.time()
-        if st.session_state.selected_db == 'user_vectordb':
-            extraction_context = user_compression_retriever.invoke(extraction_question)
-        else:
-            extraction_context = compression_retriever.invoke(extraction_question)
-        end = time.time()
-        dur = end - start
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-        print('문서 추출 소요 시간: %.2f (초)' %(dur))
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-        
-        src_doc_score = [doc.state['query_similarity_score'] for doc in extraction_context]
-        src_doc_locat = [doc.metadata for doc in extraction_context]
-        
-        sample_src_doc_df['유사도'] = src_doc_score
-        sample_src_doc_df['유사도'] = sample_src_doc_df['유사도'].round(2).astype(str)
-        sample_src_doc_df['문서 출처'] = src_doc_locat
-                
-        # 참조 문서 UI 표출
-        if sample_src_doc_df.shape[0] > 0:
-            with st.expander('참조 문서'):
-                st.table(sample_src_doc_df)
-                st.markdown("AhnLab에서 제공하는 위협정보 입니다.<br>자세한 정보는 https://www.ahnlab.com/ko/contents/asec/info 에서 참조해주세요.", unsafe_allow_html=True)
-    return prompt
