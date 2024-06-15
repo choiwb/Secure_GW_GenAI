22059661ff6fc41e8a46993927e92c6a4fc76e0e
Woobin Choi <khuam1216@gmail.com>
2024-06-15 20:31:23 +0900

    Update vqa_assistant.py

--- vqa_assistant.py
+++ vqa_assistant.py
@@ -4,10 +4,10 @@ import base64
 from io import BytesIO
 from dotenv import load_dotenv
 import streamlit as st
-from PIL import Image
 from streamlit_feedback import streamlit_feedback
 from langsmith import Client
 from langchain.callbacks.manager import collect_runs
+from pdf2image import convert_from_bytes
 
 from config import asa_image_path, you_icon, ahn_icon
 from prompt import multimodal_prompt
@@ -40,49 +40,52 @@ with st.expander('Protocol Stack'):
     
 with st.sidebar:
     st.markdown("<h3 style='text-align: center;'>이미지 업로드</h3>", unsafe_allow_html=True)
-    uploaded_image = st.file_uploader("이미지 선택", type=["png", "jpg", "jpeg"])
+    uploaded_pdf = st.file_uploader("pdf 선택", type=["pdf"])
 
-    if uploaded_image is not None:        
-        # UploadedFile의 내용을 읽어서 BytesIO 객체로 변환 및 열기
-        image = Image.open(BytesIO(uploaded_image.read()))
+    if uploaded_pdf is not None:        
+        # 멀티모달 사용 (문서에서 서명, 인감 등 추출) 위해 pdf to image 
+        pages = convert_from_bytes(uploaded_pdf.read())
         
-        # 사이드바에 이미지 표시
-        st.image(image, caption="업로드된 이미지", use_column_width=True)
-        
-        width, height = image.size 
-        # print(f"width: {width}, height: {height}, size: {width*height}")
-
-        ######################################################################        
-        # 이미지를 2 x 2 로 나누었을 때 오른쪽 하단 부분 추출
-        left = width // 2
-        top = height // 2
-        right = width
-        bottom = height
-        
-        cropped_image = image.crop((left, top, right, bottom))
-        
-        # 추출한 이미지 표시
-        st.image(cropped_image, caption="추출된 이미지 (오른쪽 하단)", use_column_width=True)
-        width, height = cropped_image.size 
-        ######################################################################
-                
-        isResized = False
-        # 5242880: Claude 3 Sonnet 의 허용 사이즈
-        while(width*height > 5242880):                    
-            width = int(width/2)
-            height = int(height/2)
-            isResized = True
-            # print(f"width: {width}, height: {height}, size: {width*height}")
+        images_base64 = []
+        for page_number, image in enumerate(pages):
+            st.markdown(f"### Page {page_number + 1}")
+            st.image(image, caption=f"업로드된 이미지 - 페이지 {page_number + 1}", use_column_width=True)
                     
-        if isResized:
-            # img = image.resize((width, height))
-            cropped_image = cropped_image.resize((width, height))
-                               
-        buffer = BytesIO()
-        # image.save(buffer, format="PNG")
-        cropped_image.save(buffer, format="PNG")
+            width, height = image.size 
+            # print(f"width: {width}, height: {height}, size: {width*height}")
 
-        img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            ######################################################################        
+            # 이미지를 2 x 2 로 나누었을 때 오른쪽 하단 부분 추출
+            left = width // 2
+            top = height // 2
+            right = width
+            bottom = height
+            
+            cropped_image = image.crop((left, top, right, bottom))
+            
+            # 추출한 이미지 표시
+            st.image(cropped_image, caption="추출된 이미지 (오른쪽 하단)", use_column_width=True)
+            width, height = cropped_image.size 
+            ######################################################################
+                    
+            isResized = False
+            # 5242880: Claude 3 Sonnet 의 허용 사이즈
+            while(width*height > 5242880):                    
+                width = int(width/2)
+                height = int(height/2)
+                isResized = True
+                # print(f"width: {width}, height: {height}, size: {width*height}")
+                        
+            if isResized:
+                # img = image.resize((width, height))
+                cropped_image = cropped_image.resize((width, height))
+                                
+            buffer = BytesIO()
+            # image.save(buffer, format="PNG")
+            cropped_image.save(buffer, format="PNG")
+
+            img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            images_base64.append(img_base64)
 
     st.markdown('<br>', unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center;'>피드백 방법</h3>", unsafe_allow_html=True)
@@ -128,14 +131,15 @@ if prompt := st.chat_input(""):
                 with collect_runs() as cb:
                     full_response = ""
                     message_placeholder = st.empty()
-                    st.session_state.image_data = uploaded_image
-                                            
-                    img_context = aws_vis_pipe.invoke(multimodal_prompt(img_base64)) 
-                    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-                    print(img_context)
-                    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+                    st.session_state.image_data = uploaded_pdf
 
-                    for chunk in aws_vis_vectordb_txt_pipe.stream({"img_context":img_context, "question":prompt}):
+                    img_context_total = []
+                    for i in images_base64:    
+                        img_context = aws_vis_pipe.invoke(multimodal_prompt(i)) 
+                        img_context_total.append(img_context)
+                    img_context_total = '\n'.join(img_context_total)
+
+                    for chunk in aws_vis_vectordb_txt_pipe.stream({"img_context":img_context_total, "question":prompt}):
                         full_response += chunk
                         message_placeholder.markdown(full_response, unsafe_allow_html=True)   
                         
