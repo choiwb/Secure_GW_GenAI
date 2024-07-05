939692e395fd0c58cc0ac92bc866f4e862a603e4
Woobin Choi <khuam1216@gmail.com>
2024-07-05 15:47:06 +0900

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -1,5 +1,7 @@
 
 
+import time
+from datetime import datetime
 import httpx
 import requests
 from typing import Any, List, Optional
@@ -13,18 +15,19 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_aws import ChatBedrock
 
-from hcx_token_cal import token_CompletionExecutor
 from prompt import sllm_inj_rag_prompt
-from config import HCX_LLM_URL, bedrock_runtime, aws_llm_id, sllm_model_path, sllm_n_batch, sllm_n_gpu_layers, hcx_general_headers, hcx_stream_headers, hcx_llm_params, llm_maxtokens, llm_temperature, gemini_llm_params, gemini_safe, sllm_n_ctx, sllm_top_p
+from config import HCX_LLM_URL, hcx_003_token_per_price, bedrock_runtime, aws_llm_id, sllm_model_path, sllm_n_batch, sllm_n_gpu_layers, hcx_general_headers, hcx_stream_headers, hcx_llm_params, llm_maxtokens, llm_temperature, gemini_llm_params, gemini_safe, sllm_n_ctx, sllm_top_p
 from streamlit_custom_func import hcx_stream_process
 from token_usage import record_token_usage
 
-token_completion_executor = token_CompletionExecutor()
  
 class HCX(LLM): 
-    # init_input_token_count: int = 0
+    question_time: str = ''
+    dur_latency: float = 0.0
     init_system_prompt: str
     streaming: bool = Field(default = False)
+    token_count = int = 0
+    token_price: float = 0.0
 
     @property
     def _llm_type(self) -> str:
@@ -45,35 +48,40 @@ class HCX(LLM):
         'messages': preset_text
         }
         total_request_data = request_data | hcx_llm_params
-
-        # total_input_token_json = token_completion_executor.execute(request_data)
-        # self.init_input_token_count = sum(token['count'] for token in total_input_token_json[:])
-
+        
+        # 질문 요청 시간
+        now_time = datetime.now()
+        self.question_time = now_time.strftime('%Y%M%d%H%M%S')
+        # LLM 요청 시간 
+        start_latency = time.time()
         if self.streaming == True:
             with httpx.stream(method="POST",
                             url=HCX_LLM_URL,
                             json=total_request_data,
                             headers=hcx_stream_headers,
                             timeout=10) as res:
-                total_count, full_response = hcx_stream_process(res)
-                print('토큰 총 사용량: ', total_count)
+                end_latency, self.token_count, full_response = hcx_stream_process(res)
+                print('토큰 총 사용량: ', self.token_count)
+                self.token_price = self.token_count * hcx_003_token_per_price
+                # 첫 토큰 지연시간
+                end_latency = time.time()
+                self.dur_latency = end_latency - start_latency
+                self.dur_latency = round(self.dur_latency, 2)
                 return full_response
             
         else:       
             response = requests.post(HCX_LLM_URL, json=total_request_data, headers=hcx_general_headers, verify=False)
-            # response.raise_for_status()     
+            # 첫 토큰 지연시간
+            end_latency = time.time()
+            self.dur_latency = end_latency - start_latency
+            self.dur_latency = round(self.dur_latency, 2)
+
             response = response.json()       
-            llm_result = response['result']['message']['content']
-            # preset_text = [{"role": "system", "content": ""}, {"role": "user", "content": llm_result}]
-            # output_token_json = {
-            #     "messages": preset_text
-            #     }
-            # total_input_token_json = token_completion_executor.execute(output_token_json)
-            # self.init_input_token_count += sum(token['count'] for token in total_input_token_json[:])
-            
-            total_count = response["result"]["inputLength"] + response["result"]["outputLength"]
-            print('토큰 총 사용량: ', total_count)
-            record_token_usage(total_count)
+            llm_result = response['result']['message']['content']            
+            self.token_count = response["result"]["inputLength"] + response["result"]["outputLength"]
+            print('토큰 총 사용량: ', self.token_count)
+            self.token_price = self.token_count * hcx_003_token_per_price
+            record_token_usage(self.token_count)
             return llm_result
 
 gpt_model = ChatOpenAI(
@@ -119,5 +127,3 @@ sllm = LlamaCpp(model_path=sllm_model_path, temperature=llm_temperature, max_tok
     n_batch=sllm_n_batch,
     use_mlock=True,
     prompt=sllm_inj_rag_prompt)
-
-
