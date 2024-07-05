6232cc3e81da8d6553b51c204e87e2cf55549d66
Woobin Choi <khuam1216@gmail.com>
2024-07-05 15:48:54 +0900

    Update streamlit_custom_func.py

--- streamlit_custom_func.py
+++ streamlit_custom_func.py
@@ -1,5 +1,6 @@
 
 
+import time
 import json
 import streamlit as st
 
@@ -7,19 +8,23 @@ import streamlit as st
 def hcx_stream_process(res):
     full_response = ""
     message_placeholder = st.empty()       
-    
+    # 첫 토큰 카운트
+    first_token_count = 0
     for line in res.iter_lines():
         if line.startswith("data:"):
             split_line = line.split("data:")
             line_json = json.loads(split_line[1])
             if "stopReason" in line_json and line_json["stopReason"] == None:
                 full_response += line_json["message"]["content"]
+                # 첫 토큰이 1인 경우, 첫 토큰 생성 시간 측정
+                first_token_count += 1
+                if first_token_count == 1:
+                    end_latency = time.time()
                 message_placeholder.markdown(full_response, unsafe_allow_html=True)
             # token length check
             elif "stopReason" in line_json and "seed" in line_json and line_json["stopReason"] == "stop_before":
-                total_count = line_json["inputLength"] + line_json["outputLength"] - 1
-    return total_count, full_response
-    # return full_response
+                token_count = line_json["inputLength"] + line_json["outputLength"] - 1
+    return end_latency, token_count, full_response
 
 def scroll_bottom():
     js = f"""
